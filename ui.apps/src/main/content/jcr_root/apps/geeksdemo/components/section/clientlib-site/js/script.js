394f79fda25bfc753f79fe6ad7a8bcdb5b455b5f
José Lucas <joselucas.freitas@webjump.com.br>
2025-03-13 11:44:06 -0300

    fix: section

--- ui.apps/src/main/content/jcr_root/apps/geeksdemo/components/section/clientlib-site/js/script.js
+++ ui.apps/src/main/content/jcr_root/apps/geeksdemo/components/section/clientlib-site/js/script.js
@@ -1,18 +1,22 @@
-let sectionLeft = document.getElementById('section_left');
-let sectionRight = document.getElementById('section_right');
-
-
 window.addEventListener('load', () => {
-  console.log(sectionLeft)
-  console.log(sectionRight)
-  if(sectionLeft === null) {
-    sectionRight.style.width = "100%";
+  let sectionLeft = document.getElementById('section_left');
+  let sectionRight = document.getElementById('section_right');
+  let centerContent = document.getElementById('center').value;
+  let border = document.getElementById('border').value;
+
+  if (sectionLeft === null) {
+    sectionRight.style.width = 100 + "%";
     sectionRight.style.paddingLeft = "0";
+  }
+
+  if(centerContent === "true"){
     sectionRight.style.display = "flex";
     sectionRight.style.flexDirection = "column";
     sectionRight.style.justifyContent = "center";
     sectionRight.style.alignItems = "center";
-  }else{
-    console.log('nao entrou')
+  }
+
+  if(border === "true"){
+    sectionLeft.style.borderRight = "1px solid black";
   }
 })
